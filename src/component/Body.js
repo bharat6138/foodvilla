6c3e0a4cf23344ef112e0da62faa2da70a5cdc39
Bharat bhushan <43534123+bharat6138@users.noreply.github.com>
2023-01-22 19:55:58 +0530

    Add files via upload

--- src/component/Body.js
+++ src/component/Body.js
@@ -4,27 +4,35 @@ import { Restaurant_Data } from "./config";
 import Skeleton from "./Skeleton";
 import { Carousel } from "./Carousel";
 import { Link } from "react-router-dom";
+import { usePosition } from "../../utils/GeoCode/usePosition";
 function filterData(searchInput, resturantsList) {
 	let searchData = resturantsList.filter((item) =>
 		item?.data?.name?.toLowerCase().includes(searchInput.toLowerCase()),
 	);
-	console.log(searchData);
+	// console.log(searchData);
 	return searchData;
 }
 
 export default function Body() {
 	const [allresturantList, setAllResturantList] = useState([]);
 	const [filteredResturantsList, setFilteredResturantsList] = useState([]);
+	const { latitude, longitude, error } = usePosition();
+	const API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${latitude}&lng=${longitude}&page_type=DESKTOP_WEB_LISTING`;
+	// const ADDRESS = `https://www.latlong.net/c/?lat=${lat}&long=${lng}`;
+
 	useEffect(() => {
-		getRestaurants();
-	}, []);
+		if (latitude && longitude) getRestaurants();
+	}, [latitude, longitude]);
+
+	// console.log(API_URL);
 	async function getRestaurants() {
-		const data = await fetch(
-			"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.717111&lng=77.157598&page_type=DESKTOP_WEB_LISTING",
-		);
+		const data = await fetch(API_URL);
+
 		const json = await data.json();
 		setAllResturantList(json?.data?.cards[2]?.data?.data?.cards);
 		setFilteredResturantsList(json?.data?.cards[2]?.data?.data?.cards);
+		console.log(API_URL);
+		// console.log(ADDRESS);
 	}
 
 	// console.log(resturantList.length);
@@ -34,7 +42,19 @@ export default function Body() {
 			<div className="body-content">
 				<div className="inner-body-content">
 					<div className="all-resturant">
-						{allresturantList.length === 0 ? (
+						<p>
+							My Location Latitude=
+							<strong style={{ color: "rgb(252, 128, 25)" }}>{latitude}</strong> and
+							Longitude=
+							<strong style={{ color: "rgb(252, 128, 25)" }}>{longitude}</strong>
+						</p>
+						<p style={{ marginTop: "20px", marginBottom: "20px" }}>
+							<strong style={{ color: "rgb(252, 128, 25)" }}>
+								Get API URL on basis of latitude longitude
+							</strong>{" "}
+							= <a href="{API_URL}">{API_URL}</a>
+						</p>
+						{allresturantList?.length === 0 ? (
 							<div className="skeleton">
 								{Array(4)
 									.fill("")
@@ -52,18 +72,15 @@ export default function Body() {
 								/>
 								<div className="resturants-margin">
 									<div className="card-row ">
-										{filteredResturantsList.map((restaurant) => {
+										{filteredResturantsList?.map((restaurant) => {
 											return (
-												<>
-													{console.log(restaurant.data.id)}
-													<Link
-														to={"/restaurant/" + restaurant.data.id}
-														className="card-item"
-														key={restaurant.data.id}
-													>
-														<Resturant {...restaurant.data} />
-													</Link>
-												</>
+												<Link
+													to={"/restaurant/" + restaurant.data.id}
+													className="card-item"
+													key={restaurant.data.id}
+												>
+													<Resturant {...restaurant.data} />
+												</Link>
 											);
 										})}
 									</div>
@@ -89,8 +106,8 @@ function Search({
 				<div className="box">
 					<div className="coloumn">
 						<div className="total-res">
-							{filteredResturantsList.length > 0
-								? `${filteredResturantsList.length} resturants`
+							{filteredResturantsList?.length > 0
+								? `${filteredResturantsList?.length} resturants`
 								: "no result found"}
 						</div>
 						<div className="search-box">
